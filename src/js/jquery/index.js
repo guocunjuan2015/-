c786023af7fc23a11b19ed500e558b02f787f482
guocunjuan <1419201498@qq.com>
2017-10-30 18:01:58 +0800

    样式修改

--- src/js/jquery/index.js
+++ src/js/jquery/index.js
@@ -11,14 +11,12 @@
         :
         $("#menuHeader").stop(!0,!0).removeClass("fixedNav").animate({top:"0"},20)
     });
+
     //个人中心创建计划模板点击效果
-  
     $(".paln_wrapper li").hover(function(event) {
       var index= $(".paln_wrapper li").index(this);
       //$(this).find(".menu-title").show();
       $(".paln_wrapper li").eq(index).addClass('selected').siblings(".paln_wrapper li").removeClass('selected');
-
-
       /* Act on the event */
    });
 
@@ -27,19 +25,15 @@
     $("#menu-toggle").click(function(event) {
 
           if(isClosed == true){
-
               $("#sidebar").addClass('is-open');
               console.log("111");
               isClosed = false;
           } else {
 
             $("#sidebar").removeClass('is-open');
-
             isClosed = true;
             console.log("222");
-
           }
-
       /* Act on the event */
  });
 
@@ -54,11 +48,7 @@
        $("#side-menu li").eq(index).addClass('nav-hover').siblings("#side-menu li").removeClass('nav-hover');
    }
 
-   /* Act on the event */
-});
-
-
-
-
+    /* Act on the event */
+  });
 
 });
